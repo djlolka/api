80fbddddb99c0b303a642c86f728739345776190
djlolka <nekrasovneketa68@gmail.com>
2022-02-08 19:35:38 +0500

    0.2

--- main.py
+++ main.py
@@ -23,6 +23,38 @@ class MapParams:
         self.dict['spn'] = ','.join([str(self.spn), str(self.spn)])
         return self.dict
 
+    def increase(self):
+        if self.spn > 0.001:
+            if 0.001 <= self.spn < 0.01:
+                self.spn -= 0.001
+            elif 0.01 <= self.spn < 0.1:
+                self.spn -= 0.01
+            elif 0.1 <= self.spn < 1:
+                self.spn -= 0.1
+            elif 1 <= self.spn < 10:
+                self.spn -= 1
+            elif 10 <= self.spn <= 90:
+                self.spn -= 10
+
+    def decrease(self):
+        if self.spn < 90:
+            if 0.001 <= self.spn < 0.01:
+                self.spn += 0.001
+            elif 0.01 <= self.spn < 0.1:
+                self.spn += 0.01
+            elif 0.1 <= self.spn < 1:
+                self.spn += 0.1
+            elif 1 <= self.spn < 10:
+                self.spn += 1
+            elif 10 <= self.spn < 90:
+                self.spn += 10
+
+    def update(self, key):
+        if key == pygame.K_PAGEUP:
+            self.decrease()
+        elif key == pygame.K_PAGEDOWN:
+            self.increase()
+
 
 def get_image(dict_params):
     api_server = 'https://static-maps.yandex.ru/1.x/'
@@ -34,18 +66,28 @@ def get_image(dict_params):
         file.write(response.content)
 
 
-def UI():
+def UI(my_map):
     pygame.init()
     screen = pygame.display.set_mode((600, 450))
     # Рисуем картинку, загружаемую из только что созданного файла.
     screen.blit(pygame.image.load(MAP_FILE), (0, 0))
     # Переключаем экран и ждем закрытия окна.
     pygame.display.flip()
-    while pygame.event.wait().type != pygame.QUIT:
-        pass
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.KEYDOWN:
+                my_map.update(event.key)
+                get_image(my_map.make_dict())
+                pygame.event.clear()
+        screen.blit(pygame.image.load(MAP_FILE), (0, 0))
+        pygame.display.flip()
+
     pygame.quit()
 
 
 my_map = MapParams(longitude, latitude, spn, 'map')
 get_image(my_map.make_dict())
-UI()
+UI(my_map)
